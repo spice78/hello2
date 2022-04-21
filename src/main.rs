2a42277423d7365fb46f07ce203fb8c1aeb96081
spice-home <spicerama@gmail.com>
2022-04-22 00:44:52 +0300

    full rand game

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
-    println!("This addition for penis!");
+    println!("Guess number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Secret number quall {}", secret_number);
+
+    loop{
+        println!("Pls, enter ur guess!");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Wrong number!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            
+
+        println!("You guess: {}", guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("U won!");
+                break;
+            }
+        }
+    }
 }
