16a884ad6a1d68dd8bd6052f13f8d7b9f57309e9
spice78 <clickerheroes2015@yandex.ru>
2022-06-23 10:59:59 +0300

    something with array math

--- src/main.rs
+++ src/main.rs
@@ -79,7 +79,7 @@ fn main(){
     println!("{}", res);
 }
 */
-
+/*
 fn main(){
     let mut count = 0;
     let mut num = 64;
@@ -99,4 +99,109 @@ fn fn_for(x:i32, mut z:i32, i:i32) {
         println!("fn_for == {}",  z);
         z -= i;
     }    
+}
+*/
+/*
+fn main() {
+    convert_fahr(315.0);
+    convert_cels(315.0);
+}
+
+fn convert_fahr (x:f32) {
+    let mut fahr:f32 = 0.0;
+
+    while x >= fahr {        
+        let celsius = (5.0/9.0)*(fahr-32.0);
+        
+        println!("cels - {}, fahr - {}", celsius, fahr);
+
+        fahr += 45.0;
+    }
+}
+
+fn convert_cels(x:f32) {
+    let mut cels = 0.0;
+
+    while x >= cels {
+        let fahr = ((9.0/5.0) * cels) + 32.0;
+
+        println!("cels - {}, fahr - {}", cels, fahr);
+        
+        cels += 45.0;
+    }
+}
+*/
+//===================================
+/*
+use std::ops::Neg;
+
+fn main() {
+    let num = 2;
+
+    if num >= 2 {
+        finn_plus(num);
+    } else if num < 0 {
+        finn_neg(num);        
+    } else {
+        println!("y {} num 1", num);
+    }
+}
+
+fn finn_plus(x:i32) {
+    //let mut num = 1;
+    let mut num_f = 0;
+    let mut num_0 = 1;
+    let mut num_1 = 0;
+
+    for y in 1..(x+1) {
+        
+        let num = num_0 + num_1;
+        num_0 = num;
+        num_1 = num_f;
+        num_f = num;
+    println!("y {}, num {}", y, num);
+    }    
+}
+
+fn finn_neg(x:i32) {
+    //let mut num = 1;
+    let mut num_f = 0;
+    let mut num_0 = 1;
+    let mut num_1 = 0;
+
+    for y in 1..(x-1).neg() {
+        
+        let num = num_1 - num_0;
+        num_0 = num;
+        num_1 = num_f;
+        num_f = num;
+    println!("y {}, num {}", y.neg(), num.neg());
+    }
+}
+*/
+
+fn main () {
+    //let s = "hello".to_owned();
+    //let w = "world!";
+
+    //println!("{}", s);
+
+    //let s2 = s + " " + w;
+    //println!("{}", s2);
+
+    let i = &mut [1, 2, 3, 5];
+    let r = &[4, 5, 6, 5];
+
+    let mut k = 0;
+    let mut y = &r[k];
+
+        for j in i.iter_mut() {                                                    
+                *j += y;
+                k += 1;                    
+                if k == r.len() {
+                    k -= 1;
+                    y = &0;
+                } else { y = &r[k]; }                                                  
+        } 
+        println!("{:?}", i);  
 }
